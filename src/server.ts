087182921152bc7ff7ca668f7a163206df59fac8
matiasArrebillaga <matiasarrebillaga@gmail.com>
2026-08-19 19:48:35 -0300

    added prisma connection

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,16 @@
 import app from "./app";
+import { AppDataSource } from "./config/data-source";
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Conectado a MySQL");
+
+        app.listen(PORT, () => {
+            console.log(`Servidor corriendo en http://localhost:${PORT}`);
+        });
+    })
+    .catch((error: unknown) => {
+        console.error("Error al conectar con MySQL:", error);
+    });
