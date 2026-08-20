3ef1a88087d2617a8c516d4c1f4269354cceae2d
matiasArrebillaga <matiasarrebillaga@gmail.com>
2026-08-20 17:33:42 -0300

    fixed prisma connection and changed to tsx

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,21 @@
 import app from "./app";
-
+import { prisma } from "./config/prisma";
 
 const PORT = 3000;
 
+async function startServer() {
+    try {
+        await prisma.$connect();
+
+        console.log("Conectado a MySQL mediante Prisma");
+
+        app.listen(PORT, () => {
+            console.log(`Servidor corriendo en http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.error("Error al conectar con la base de datos:", error);
+        process.exit(1);
+    }
+}
+
+startServer();
