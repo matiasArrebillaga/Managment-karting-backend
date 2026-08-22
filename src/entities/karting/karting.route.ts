2be7a4cac76a2aac70202dbf66900eb2576d3551
matiasArrebillaga <matiasarrebillaga@gmail.com>
2026-08-22 20:30:56 -0300

    added karting controller

---
+++ src/entities/karting/karting.route.ts
@@ -0,0 +1,27 @@
+import {Request , Response} from "express";
+import { KartingService } from "./karting.service";
+
+const kartingService = new KartingService();
+
+export class kartingController{
+    async getAll(req: Request, res: Response){
+        try{
+            const kartings = await kartingService.getAll();
+            res.json(kartings);
+        }catch (error){
+            res.status(500).json({
+                message: "Error al obtener los kartings"
+            });
+        }
+    }
+    async getById (req: Request, res: Response){
+        try{
+            const karting= await kartingService.getById();
+            res.json(karting);
+        }catch(error){
+            res.status(500).json({
+               message: "Error al obtener el karting"
+            });
+        }
+    }
+}
