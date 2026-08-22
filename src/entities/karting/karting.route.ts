03a92371d5984cc0841dce64c5a5246349d7f5d6
matiasArrebillaga <matiasarrebillaga@gmail.com>
2026-08-22 20:38:18 -0300

    added karting route

--- src/entities/karting/karting.route.ts
+++ src/entities/karting/karting.route.ts
@@ -1,34 +1,11 @@
-import {Request , Response} from "express";
-import { KartingService } from "./karting.service";
+import { Router } from "express";
+import { kartingController } from "./karting.controller";
 
-const kartingService = new KartingService();
+const router = Router();
 
-export class kartingController{
-    async getAll(req: Request, res: Response){
-        try{
-            const kartings = await kartingService.getAll();
-            res.json(kartings);
-        }catch (error){
-            res.status(500).json({
-                message: "Error al obtener los kartings"
-            });
-        }
-    }
-    async getById (req: Request, res: Response){
-        try{
-            const id = Number(req.params.idKartings)
-            const karting= await kartingService.getById(id);
-            
-            if (!karting){
-                return res.status(404).json({
-                    message: "Karting no encontrado"
-                });
-            }
-            res.json(karting);
-        }catch(error){
-            res.status(500).json({
-               message: "Error al obtener el karting"
-            });
-        }
-    }
-}
+const controller = new kartingController();
+
+router.get("/", controller.getAll.bind(controller));
+router.get("/id", controller.getById.bind(controller));
+
+export default router;
