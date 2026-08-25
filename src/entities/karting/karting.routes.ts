0f825633dffae6003c369d5a7dc3e57ad8a6b16b
matiasArrebillaga <matiasarrebillaga@gmail.com>
2026-08-25 04:07:33 -0300

    added full karting CRUD controller

--- src/entities/karting/karting.routes.ts
+++ src/entities/karting/karting.routes.ts
@@ -6,5 +6,7 @@ const router = Router();
 
 router.get("/", kartingController.getAll);
 router.get("/:id", kartingController.getById);
-
+router.post("/", kartingController.create);
+router.put("/:id", kartingController.update);
+router.delete("/:id",kartingController.delete);
 export default router;
