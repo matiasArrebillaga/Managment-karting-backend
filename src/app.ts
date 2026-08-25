b8ceddc3dae6cc2937742140e8b45a8cf53a02cb
matiasArrebillaga <matiasarrebillaga@gmail.com>
2026-08-25 17:22:21 -0300

    added persona

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from "express"
 import kartingRoutes from "./entities/karting/karting.routes"
+import personaRoutes from "./entities/persona/persona.routes"
 const app = express();
 app.use(express.json());
 app.use("/api/kartings", kartingRoutes);
+app.use("/api/personas",personaRoutes);
 app.get("/", (req, res) => {
     res.send("API funcionando");
 });
