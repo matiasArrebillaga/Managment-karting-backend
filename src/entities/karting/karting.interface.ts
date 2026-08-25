a75a7ab931844f30fa4421a27a93cbf815d59c4e
matiasArrebillaga <matiasarrebillaga@gmail.com>
2026-08-25 03:54:31 -0300

    added karting interface

---
+++ src/entities/karting/karting.interface.ts
@@ -0,0 +1,8 @@
+export interface IKarting {
+    idKartings: number;
+    categoria: string;
+    modelo: string;
+    estado: string;
+    fechaAdquisicion: Date;
+    TiposKarting_idTiposKarting: number;
+}
