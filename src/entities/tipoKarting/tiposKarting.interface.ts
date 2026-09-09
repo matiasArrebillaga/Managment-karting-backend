cbc1678ba445d9470730a9d847c02fa6ab09da15
matiasArrebillaga <matiasarrebillaga@gmail.com>
2026-09-09 08:02:28 -0300

    changed interface to match schema

--- src/entities/tipoKarting/tiposKarting.interface.ts
+++ src/entities/tipoKarting/tiposKarting.interface.ts
@@ -1,4 +1,9 @@
-export interface ITiposKartings { idTipoKarting?: number; nombre: string; descripcion: string; }
+export interface ITiposKartings {
+    idTipoKarting?: number;
+    nombre: string;
+    descripcion: string;
+    TiposLicencias_idTipoLicenciaMinima: number;
+}
 
 export type CreateTiposKartings = Omit<ITiposKartings, "idTipoKarting">;
 export type UpdateTiposKartings = Partial<CreateTiposKartings>;
